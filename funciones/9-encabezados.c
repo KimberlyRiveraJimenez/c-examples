11c983a6102c96d50a73fc7be7e372937dfa57f9
KimberlyRivera <kimberly.riverajimenez@ucr.ac.cr>
2024-09-19 02:18:36 -0600

    Modificacion_de_funcion5

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -3,15 +3,17 @@
 
 int main() {
 
-    int v;
+    int v, a;
 
     // Llama a las 3 funciones
     funcion1();
     funcion2(5, 2.67);
     v = funcion3(10);
+    a = funcion5(10);
 
-    // Imprime el valor que retornó funcion3()
+    // Imprime el valor que retornó funcion3() y funcion5()
     printf("v = %d\n", v);
+    printf("a = %d\n", a);
 
     return 0;
 
@@ -36,9 +38,13 @@ int funcion3(int x) {
     return y;
 }
 
-int funcion5(int x) {
-    printf("en funcion5");
 int funcion4(int x) {
     printf("En funcion4()");
     return x;
+}
+
+int funcion5(int x) {
+    printf("En funcion5()...\n");
+    int y = x + 3;
+    return y;
 }
